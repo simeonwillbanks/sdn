1e4e6a50f87df961050025c1e6811cdea1d7d870
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2011-09-22 20:29:56 -0700

    railsify

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdn::Application.config.secret_token = '858ef458d9291d46b969b9707422cf15f1bc5d96d739236ce9200272c8c9140e2de40a1978672da2a4836da2de37737f8e2fa3c1e43a45b1aa329c9de31c1e66'
