b515b6bfc91cd9d037847ffa7bb22f2c077dc5fd
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2011-10-31 09:01:48 -0700

    Add user and admin seeds for manual browser testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+password = "password123"
+admin = User.create(:email => "admin@test.tld", :password => password, :password_confirmation => password)
+admin.confirm!
+admin.reset_authentication_token!
+user = User.create(:email => "user@test.tld", :password => password, :password_confirmation => password)
+user.confirm!
