dc668841040c35a43836a48a1fffdadb51e04350
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2011-11-04 19:52:13 -0700

    Add Momentable library to encapsulate controller logic for moment types.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,3 @@
 class PostsController < InheritedResources::Base
-  prepend_before_filter :authenticate_user!
-  before_filter :authenticate_admin!
-  actions :show, :create 
-  respond_to :json
-  respond_to :html, :only => :show
+  include Momentable
 end
---
+++ lib/momentable.rb
@@ -0,0 +1,16 @@
+module Momentable
+  extend ActiveSupport::Concern
+  included do
+    prepend_before_filter :authenticate_user!
+    before_filter :authenticate_admin!
+    actions :show, :create 
+    respond_to :json
+    respond_to :html, :only => :show
+  end
+  module InstanceMethods
+    protected
+      def authenticate_admin!
+        render :file => "public/401.html", :status => :unauthorized unless user_signed_in? and current_user.admin?
+      end
+  end
+end
