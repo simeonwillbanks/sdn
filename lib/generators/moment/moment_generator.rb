d2dc14a509a63b4d787ac65d1b81d01da3ccf60b
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2011-11-22 14:37:23 -0800

    Whoops, syntax error

--- lib/generators/moment/moment_generator.rb
+++ lib/generators/moment/moment_generator.rb
@@ -70,12 +70,12 @@ end
   end
 
   def features 
-    %w(home_page, moments_display, moment_creation).each do |f|
+    %w(home_page moments_display moment_creation).each do |f|
       insert_into_file "features/#{f}.feature", :after => example_header(['moment','attribute','value']) do
         example_row [singular_name, moment_attribute, sentence]
       end
     end
-    %w(moment_comments, moments_display, moment_creation).each do |f|
+    %w(moment_comments moments_display moment_creation).each do |f|
       insert_into_file "features/#{f}.feature", :after => example_header(['moment']) do 
         example_row [singular_name]
       end
